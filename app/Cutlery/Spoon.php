b3db321066ded43a762f64b8533e19a6207762be
Georgia Harrison <georgiaharrison1994@gmail.com>
2021-02-12 12:16:45 +0000

    Challenge 09-01

---
+++ app/Cutlery/Spoon.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+namespace App\Cutlery;
+
+class Spoon
+{
+    protected $scoops;
+
+    public function __construct()
+    {
+        $this->scoops = 0;
+    }
+
+    public function scoop() : Spoon
+    {
+        $this->scoops += 1;
+        return $this;
+    }
+
+    public function howManyScoops() : int
+    {
+        return $this->scoops;
+    }
+}
